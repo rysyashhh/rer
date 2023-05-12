af222256813c9a0c4dc252f7eb2d56b158f33b43
Arina Ryseva <arina.ryseva2512@gmail.ru>
2023-05-12 21:07:39 +0300

    44 тема

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// Найти нужные узлы
+const popupButton = document.querySelector ('popup-menu__button');
+const popupWrapper = document.querySelector('popup-menu')
+const popupMenu = document.querySelector ('popup-menu__list');
+
+//Выполним функцию клика по кнопке
+
+popupButton.addEventListener ('click', handleMenu);
+
+//Функция, которая показывает и скрывает поп-ап
+function handleMenu () {
+    popupMenu.classList.toggle('hide-popup');
+}
+
+// Функция при клике на любое место экрана
+document.addEventListener('click', hidePopup);
+
+function hidePopup(el) {
+    //Проверяем, есть ли внутри того, по чему кликнули поп-ап меню или кнопка
+    let targetInside = popupWrapper.contains(el.target);
+    //Если нет, то прячем меню
+    if(!targetInside) {
+        popupMenu.classList.add('hide-popup');
+    }
+    else {
+        return;
+    }
+}
